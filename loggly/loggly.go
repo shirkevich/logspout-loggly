a154eedad0f38354ee21ab404f3243574db08689
Mikhail Shirkov <shirkevich@gmail.com>
2016-02-16 01:33:02 +0300

    skip defer

--- loggly/loggly.go
+++ loggly/loggly.go
@@ -108,7 +108,7 @@ func (l *Adapter) SendMessage(msg logglyMessage) {
 
 func (l *Adapter) sendRequestToLoggly(req *http.Request) {
 	resp, err := l.client.Do(req)
-	defer resp.Body.Close()
+	// defer resp.Body.Close()
 
 	if err != nil {
 		l.log.Println(
